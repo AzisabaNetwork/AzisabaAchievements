a7b264596bc68e779763c29bb0e67bbdb83f65b0
acrylic-style <me@acrylicstyle.xyz>
2022-11-24 03:31:57 +0900

    feat: send message to the player and play effect when achievement is unlocked

--- spigot/src/main/java/net/azisaba/azisabaachievements/spigot/network/SpigotPacketListener.java
+++ spigot/src/main/java/net/azisaba/azisabaachievements/spigot/network/SpigotPacketListener.java
@@ -3,15 +3,40 @@ package net.azisaba.azisabaachievements.spigot.network;
 import net.azisaba.azisabaachievements.api.AzisabaAchievementsProvider;
 import net.azisaba.azisabaachievements.api.achievement.AchievementData;
 import net.azisaba.azisabaachievements.api.network.ServerPacketListener;
+import net.azisaba.azisabaachievements.api.network.packet.PacketCommonAchievementUnlocked;
 import net.azisaba.azisabaachievements.api.network.packet.PacketServerCreateAchievementCallback;
 import net.azisaba.azisabaachievements.api.network.packet.PacketServerFetchAchievementCallback;
 import net.azisaba.azisabaachievements.spigot.achievement.SpigotAchievementManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.EntityEffect;
+import org.bukkit.FireworkEffect;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public class SpigotPacketListener implements ServerPacketListener {
+    @Override
+    public void handle(@NotNull PacketCommonAchievementUnlocked packet) {
+        Player player = Bukkit.getPlayer(packet.getPlayerUniqueId());
+        if (player == null) {
+            return;
+        }
+        // TODO: hardcoded message
+        player.sendMessage(ChatColor.GREEN + "実績解除！ 「" + ChatColor.YELLOW + packet.getAchievement().getKey() + ChatColor.GREEN + "」");
+        Firework firework = player.getWorld().spawn(player.getLocation(), Firework.class);
+        FireworkMeta meta = firework.getFireworkMeta();
+        meta.getEffects().add(FireworkEffect.builder().withColor(Color.GREEN).withFade(Color.RED).build());
+        meta.setPower(0);
+        firework.setFireworkMeta(meta);
+        firework.playEffect(EntityEffect.FIREWORK_EXPLODE);
+    }
+
     @Override
     public void handle(@NotNull PacketServerCreateAchievementCallback packet) {
         CompletableFuture<AchievementData> cb = ((SpigotAchievementManager) AzisabaAchievementsProvider.get().getAchievementManager())
